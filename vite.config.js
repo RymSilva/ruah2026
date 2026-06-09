bfd04146e6edc157c6946ff5f8b5db09de0308b4
Ryan Marcos <marcosdasilvaryan34@gmail.com>
2026-06-09 20:24:43 -0300

    fix: include tslib in bundle

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  resolve: {
-    alias: {
-      tslib: "tslib",
-    },
+  optimizeDeps: {
+    include: ["tslib"],
   },
 });
